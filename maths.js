2a6e828871f97a0736a9c7a74609510e5e1f13e9
dan alldredge <dalld87@gmail.com>
2015-10-01 22:42:42 -0700

    QUnit
    Added QUnit tests. Updated multiplinus

--- maths.js
+++ maths.js
@@ -2,7 +2,12 @@ var maths = (function () {
 	var math = {};
 	
 	math.multiplinus = function (x, y) {
-		return (x - 1) * y;
+		if (y > -1) {
+			return x - y;
+		}
+		else {
+			return (x - 1) * Math.abs(y);
+		}
 	};
 	
 	return math;
---
+++ test/test.js
@@ -0,0 +1,4 @@
+QUnit.test("maths.multiplinus basics", function (assert) {
+	assert.equal(maths.multiplinus(10, -6), 54, "54 test");
+	assert.equal(maths.multiplinus(10, 6), 4, "4 test");
+});
